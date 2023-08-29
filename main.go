7965bcd8690841973cb5dde6b1bc0b9508511bae
sumetsm <sumetssr@gmail.com>
2023-08-29 21:08:07 +0700

    0303

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"ezcom/routes"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,8 +17,22 @@ func main() {
 	routes.Setup(router)
 
 	// Start the server
-	err := router.Run(":3000")
+	// Returns PORT from environment if found, defaults to
+	// value in `port` parameter otherwise. The returned port
+	// is prefixed with a `:`, e.g. `":3000"`.
+
+	var port = envPortOr("0303")
+	err := router.Run(port)
 	if err != nil {
 		log.Fatal("Failed to start the server: ", err)
 	}
+}
+
+func envPortOr(port string) string {
+	// If `PORT` variable in environment exists, return it
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		return ":" + envPort
+	}
+	// Otherwise, return the value of `port` variable from function argument
+	return ":" + port
 }
