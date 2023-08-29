13e08d91425009394fcce8a77c42ce1f4579e25e
sumetsm <sumetssr@gmail.com>
2023-08-29 20:50:42 +0700

    clean code

--- routes/routes.go
+++ routes/routes.go
@@ -10,6 +10,9 @@ import (
 // Setup initializes the routes and handlers
 func Setup(router *gin.Engine) {
 	router.GET("/test", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"Text": "Test ja"})
+	})
+	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"Text": "Hello"})
 	})
 }
