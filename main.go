8e62bec9b2d64bcea48f3758b859e38c53f56aa3
sumetsm <sumetssr@gmail.com>
2023-08-29 21:00:31 +0700

    chang port 3000

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	routes.Setup(router)
 
 	// Start the server
-	err := router.Run(":8080")
+	err := router.Run(":3000")
 	if err != nil {
 		log.Fatal("Failed to start the server: ", err)
 	}
