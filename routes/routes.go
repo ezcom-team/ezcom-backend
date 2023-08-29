11bcd4c802b263e25eb176fbaca4c6ae5e2f6113
sumetsm <sumetssr@gmail.com>
2023-08-29 19:57:48 +0700

    add test api

--- routes/routes.go
+++ routes/routes.go
@@ -4,12 +4,16 @@ package routes
 import (
 	"ezcom/handlers"
 	"ezcom/middleware"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 // Setup initializes the routes and handlers
 func Setup(router *gin.Engine) {
+	router.GET("/test", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"Text": "Hello"})
+	})
 	productGroup := router.Group("/products")
 	{
 		productGroup.POST("", handlers.UploadImage)
