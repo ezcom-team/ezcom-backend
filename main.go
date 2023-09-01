cd31365b4153304ce2c9df2c359253979e0f9a92
sumetsm <sumetssr@gmail.com>
2023-09-02 00:27:59 +0700

    fix access CORS Authorization

--- main.go
+++ main.go
@@ -25,6 +25,8 @@ func main() {
 	// Set up CORS middleware
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"http://localhost:5173", "http://127.0.0.1:5173"} // แก้ไข URL ของโดเมน React ของคุณตรงนี้
+	config.AllowHeaders = []string{"Authorization"}                                  // เพิ่ม Request header ที่อนุญาตใน Preflight Response
+
 	router.Use(cors.New(config))
 
 	routes.Setup(router)
