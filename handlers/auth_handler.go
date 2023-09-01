12532405c22247997ae3ad94ad6f998917b5146f
sumetsm <sumetssr@gmail.com>
2023-09-02 02:00:00 +0700

    non secure http only

--- handlers/auth_handler.go
+++ handlers/auth_handler.go
@@ -91,7 +91,7 @@ func Login(c *gin.Context) {
 	}
 	// // set in token
 	// c.SetSameSite(http.SameSiteNoneMode)
-	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", true, false)
+	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", false, true)
 	// return response
 	c.JSON(http.StatusOK, gin.H{"token": tokenString})
 }
