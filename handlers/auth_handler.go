483d28d1b19b0470873f6864a80b5998ae706b88
sumetsm <sumetssr@gmail.com>
2023-09-02 11:48:54 +0700

    fix path set cookies

--- handlers/auth_handler.go
+++ handlers/auth_handler.go
@@ -91,7 +91,7 @@ func Login(c *gin.Context) {
 	}
 	// // set in token
 	c.SetSameSite(http.SameSiteLaxMode)
-	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", false, true)
+	c.SetCookie("Authorization", tokenString, 3600*24*30, "/", "", false, true)
 	// return response
 	c.JSON(http.StatusOK, gin.H{"token": tokenString})
 }
