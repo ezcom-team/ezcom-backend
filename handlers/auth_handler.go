f9d67d6ad70c3682a2f0ae0785245eb9c15029ce
sumetsm <sumetssr@gmail.com>
2023-09-02 11:56:20 +0700

    samesite none

--- handlers/auth_handler.go
+++ handlers/auth_handler.go
@@ -90,7 +90,7 @@ func Login(c *gin.Context) {
 		return
 	}
 	// // set in token
-	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetSameSite(http.SameSiteNoneMode)
 	c.SetCookie("Authorization", tokenString, 3600*24*30, "/", "", false, true)
 	// return response
 	c.JSON(http.StatusOK, gin.H{"token": tokenString})
