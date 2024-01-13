c1547f7724719048a5ab0bd660b44111c111ed4a
sumetsm <sumetssr@gmail.com>
2024-01-13 18:56:54 +0700

    order service

--- handlers/order_handler.go
+++ handlers/order_handler.go
@@ -6,7 +6,6 @@ import (
 	"ezcom/models"
 	"fmt"
 	"log"
-	"math"
 	"net/http"
 	"time"
 
@@ -23,8 +22,7 @@ func CreateSellOrder(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
 		return
 	}
-	todo //check ข้อมูลฃ
-	// - ดึง buyorder ที่ราคามากกว่าขึ้นไป
+
 	//ดึงค่า user จากใน context
 	user, exists := c.Get("user")
 	if !exists {
@@ -38,46 +36,81 @@ func CreateSellOrder(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Internal Server Error"})
 		return
 	}
-	sellOrder.Seller_id = userObj.ID.Hex()
-	sellOrder.CreatedAt = time.Now()
-
-	//สร้างข้อมูลใน DB
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	var collection = db.GetSellOrder_Collection()
-	result, err := collection.InsertOne(ctx, sellOrder)
+	//check ข้อมูลฃ
+	// - ดึง buyorder ที่ราคามากกว่าขึ้นไป (1 of 1)
+	collection := db.GetBuyOrder_Collection()
+	var buyOrder models.BuyOrder
+	match := true
+	filter := bson.M{"price": bson.M{"%gte": sellOrder.Price}}
+	err := collection.FindOne(context.Background(), filter).Decode(&buyOrder)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create product"})
-		return
-	}
-	// update product increase Quantity
-	var foundProduct models.Product
-	productObjID, err := primitive.ObjectIDFromHex(sellOrder.Product_id)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid product ID"})
-		return
-	}
-	foundProduct, err = models.GetProduct(productObjID)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to find product"})
-		return
-	}
-	if sellOrder.Price < foundProduct.Price {
-		err = models.UpdateProductQuantity(productObjID, foundProduct.Quantity+1, sellOrder.Price)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, err)
+		if err == mongo.ErrNoDocuments {
+			match = false
 		}
+		panic(err)
+	}
+	if match {
+		sellOrder.Seller_id = userObj.ID.Hex()
+		sellOrder.CreatedAt = time.Now()
+		// create matchedOder
+		var matchedOrder models.MatchedOrder
+		matchedOrder.Buyer_id = userObj.ID.Hex()
+		matchedOrder.Seller_id = sellOrder.Seller_id
+		matchedOrder.Color = sellOrder.Color
+		matchedOrder.Condition = sellOrder.Condition
+		matchedOrder.Price = sellOrder.Price
+		matchedOrder.Product_id = sellOrder.Product_id
+		matchedOrder.CreatedAt = time.Now()
+		collection = db.GetMatchOrder_Collection()
+		result, err := collection.InsertOne(context.Background(), matchedOrder)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		} else {
+			c.JSON(http.StatusOK, matchedOrder)
+		}
+		c.JSON(http.StatusCreated, result)
 	} else {
-		err = models.UpdateProductQuantity(productObjID, foundProduct.Quantity+1, foundProduct.Price)
+		var sellOrder models.SellOrder
+		sellOrder.Seller_id = userObj.ID.Hex()
+		sellOrder.CreatedAt = time.Now()
+		//สร้างข้อมูลใน DB
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		collection = db.GetSellOrder_Collection()
+		result, err := collection.InsertOne(ctx, sellOrder)
 		if err != nil {
-			c.JSON(http.StatusBadRequest, err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create product"})
+			return
 		}
+		// update product increase Quantity
+		var foundProduct models.Product
+		productObjID, err := primitive.ObjectIDFromHex(sellOrder.Product_id)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid product ID"})
+			return
+		}
+		foundProduct, err = models.GetProduct(productObjID)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to find product"})
+			return
+		}
+		if sellOrder.Price < foundProduct.Price {
+			err = models.UpdateProductQuantity(productObjID, foundProduct.Quantity+1, sellOrder.Price)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, err)
+			}
+		} else {
+			err = models.UpdateProductQuantity(productObjID, foundProduct.Quantity+1, foundProduct.Price)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, err)
+			}
+		}
+
+		//ส่งค่ากลับไปให้ client
+		c.JSON(http.StatusCreated, result)
 	}
 
-	//ส่งค่ากลับไปให้ client
-	c.JSON(http.StatusCreated, result)
 }
-
 func CreateBuyOrder(c *gin.Context) {
 	//รับ body
 	var buyOrder models.BuyOrder
@@ -112,10 +145,8 @@ func CreateBuyOrder(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Internal Server Error"})
 		return
 	}
-	
+
 	if match {
-		sellOrder.Seller_id = userObj.ID.Hex()
-		sellOrder.CreatedAt = time.Now()
 		// create matchedOder
 		var matchedOrder models.MatchedOrder
 		matchedOrder.Buyer_id = userObj.ID.Hex()
@@ -126,31 +157,27 @@ func CreateBuyOrder(c *gin.Context) {
 		matchedOrder.Product_id = sellOrder.Product_id
 		matchedOrder.CreatedAt = time.Now()
 		collection = db.GetMatchOrder_Collection()
-		_,err = collection.InsertOne(context.Background(),matchedOrder)
+		result, err := collection.InsertOne(context.Background(), matchedOrder)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError,gin.H{"error":err})
-			return
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
 		} else {
-			c.JSON(http.StatusOK,matchedOrder)
-			return 
+			c.JSON(http.StatusOK, matchedOrder)
 		}
+		c.JSON(http.StatusCreated, result)
 	} else {
-		todo// - ดึงข้อมูล user
 		var buyOrder models.BuyOrder
+		buyOrder.Buyer_id = userObj.ID.Hex()
+		buyOrder.CreatedAt = time.Now()
 		collection = db.GetBuyOrder_Collection()
-		_,err = collection.InsertOne(context.Background(),buyOrder)
+		result, err := collection.InsertOne(context.Background(), buyOrder)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError,gin.H{"error":err})
-			return
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
 		} else {
-			c.JSON(http.StatusOK,buyOrder)
-			return 
+			c.JSON(http.StatusOK, buyOrder)
 		}
+		c.JSON(http.StatusCreated, result)
 	}
-	
-	//return to client
 }
-
 func GetSellOrdersByUID(c *gin.Context) {
 	//ดึงค่า user จากใน context
 	user, exists := c.Get("user")
@@ -192,4 +219,99 @@ func GetSellOrdersByUID(c *gin.Context) {
 
 	// Return filtered users as JSON
 	c.JSON(http.StatusOK, sellOrders)
+}
+func GetBuyOrdersByUID(c *gin.Context) {
+	//ดึงค่า user จากใน context
+	user, exists := c.Get("user")
+	if !exists {
+		// ไม่พบค่า "user" ใน context
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized"})
+		return
+	}
+	// แปลง user เป็น models.User
+	userObj, ok := user.(models.User)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Internal Server Error"})
+		return
+	}
+	fmt.Print(userObj.ID.Hex())
+	filter := bson.M{"buyer_id": userObj.ID.Hex()}
+
+	// MongoDB query to find users by uid and type
+	collection := db.GetBuyOrder_Collection()
+	cursor, err := collection.Find(context.TODO(), filter)
+	if err != nil {
+		log.Fatal(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+	defer cursor.Close(context.TODO())
+
+	// Iterate through the results and decode them into the users slice
+	var buyOrders []models.BuyOrder
+	for cursor.Next(context.TODO()) {
+		var buyOrder models.BuyOrder
+		if err := cursor.Decode(&buyOrder); err != nil {
+			log.Fatal(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			return
+		}
+		buyOrders = append(buyOrders, buyOrder)
+	}
+
+	// Return filtered users as JSON
+	c.JSON(http.StatusOK, buyOrders)
+}
+func GetSellOrders(c *gin.Context) {
+
+	// MongoDB query to find users by uid and type
+	collection := db.GetSellOrder_Collection()
+	cursor, err := collection.Find(context.TODO(), bson.M{})
+	if err != nil {
+		log.Fatal(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+	defer cursor.Close(context.TODO())
+
+	// Iterate through the results and decode them into the users slice
+	var sellOrders []models.SellOrder
+	for cursor.Next(context.TODO()) {
+		var sellOrder models.SellOrder
+		if err := cursor.Decode(&sellOrder); err != nil {
+			log.Fatal(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			return
+		}
+		sellOrders = append(sellOrders, sellOrder)
+	}
+
+	// Return filtered users as JSON
+	c.JSON(http.StatusOK, sellOrders)
+}
+func GetBuyOrders(c *gin.Context) {
+	// MongoDB query to find users by uid and type
+	collection := db.GetBuyOrder_Collection()
+	cursor, err := collection.Find(context.TODO(), bson.M{})
+	if err != nil {
+		log.Fatal(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+	defer cursor.Close(context.TODO())
+
+	// Iterate through the results and decode them into the users slice
+	var buyOrders []models.BuyOrder
+	for cursor.Next(context.TODO()) {
+		var buyOrder models.BuyOrder
+		if err := cursor.Decode(&buyOrder); err != nil {
+			log.Fatal(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			return
+		}
+		buyOrders = append(buyOrders, buyOrder)
+	}
+
+	// Return filtered users as JSON
+	c.JSON(http.StatusOK, buyOrders)
 }
