f0d4be0e0dd3aee0f0d43c5845ca14e9838e06ab
sumetsm <sumetssr@gmail.com>
2024-01-21 13:23:56 +0700

    default

--- handlers/auth_handler.go
+++ handlers/auth_handler.go
@@ -136,9 +136,8 @@ func Login(c *gin.Context) {
 		return
 	}
 	// // set in token
-	c.SetSameSite(http.SameSiteDefaultMode)
-	c.SetCookie("Authorization", tokenString, 120000, "/", "", false, false)
-	// c.SetCookie("Authorization", tokenString, 100000, "/", "", true, true)
+	c.SetSameSite(http.SameSiteNoneMode)
+	c.SetCookie("Authorization", tokenString, 100000, "/", "", true, true)
 
 	// return response
 	// c.JSON(http.StatusOK, gin.H{"token": tokenString})
