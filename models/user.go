8dfa033a6069923fde44b7827676c52bb5b2f282
sumetsm <sumetssr@gmail.com>
2024-02-21 20:19:25 +0700

    require

--- models/user.go
+++ models/user.go
@@ -4,10 +4,10 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
 	ID       primitive.ObjectID `bson:"_id,omitempty"`
-	Name     string             `bson:"name"`
-	Email    string             `bson:"email"`
-	Password string             `bson:"password"`
-	Role     string             `bson:"role"`
+	Name     string             `bson:"name" binding:"required"`
+	Email    string             `bson:"email" binding:"required"`
+	Password string             `bson:"password" binding:"required"`
+	Role     string             `bson:"role" binding:"required"`
 	Point    float64            `bson:"point"`
 	File     string             `bson:"file"`
 }
