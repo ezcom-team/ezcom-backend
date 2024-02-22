72c6f2dd3ec7987d9b7a05f88f4fe5590eeb3fad
sumetsm <sumetssr@gmail.com>
2024-02-23 01:43:56 +0700

    multipathfile

--- models/product.go
+++ models/product.go
@@ -1,17 +1,22 @@
 // models/product.go
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"mime/multipart"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 // Product struct represents a product in the database
 type Product struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty"`
-	Name     string             `bson:"name" binding:"required"`
-	Desc     string             `bson:"desc" `
-	Price    float64            `bson:"price" binding:"required"`
-	Image    string             `bson:"image" `
-	Quantity int64              `bson:"quantity"`
-	Type     string             `bson:"type" binding:"required"`
-	Color    []string           `bson:"color" binding:"required"`
-	Specs    string             `bson:"specs"`
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Name      string             `bson:"name" binding:"required"`
+	Desc      string             `bson:"desc" `
+	Price     float64            `bson:"price" binding:"required"`
+	Image     multipart.File     `bson:"image" `
+	ImagePath string             `bson:"imagePath" `
+	Quantity  int64              `bson:"quantity"`
+	Type      string             `bson:"type" binding:"required"`
+	Color     []string           `bson:"color" binding:"required"`
+	Specs     string             `bson:"specs"`
 }
